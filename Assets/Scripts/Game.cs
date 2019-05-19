e4e626ebeeee140f022f48550c03262ffe498de4
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-19 16:59:10 -0400

    Game case prefab added

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Game", menuName = "Game")]
 public class Game : ScriptableObject {
 
+    /* PROPERTIES OF THE GAME */
+
     public string strCaseName;
     public string strDiscName;
 
     public Sprite cover;
 
-    public bool isMatch;
+    /// <summary>
+    /// Checks if the game inside the case matches the case
+    /// </summary>
+    /// <returns></returns>
+    public bool IsMatch() {
+        if (strCaseName == strDiscName) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
---
+++ Assets/Scripts/GameDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDisplay : MonoBehaviour
+{
+    public Game game;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
