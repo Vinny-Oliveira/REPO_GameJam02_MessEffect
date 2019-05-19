409798d4470b7709d367523c81eae982b16f6f09
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-19 15:43:21 -0400

    Created scriptable object for the game cases.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Game", menuName = "Game")]
+public class Game : ScriptableObject {
+
+    public string strCaseName;
+    public string strDiscName;
+
+    public Sprite cover;
+
+    public bool isMatch;
+
+}
