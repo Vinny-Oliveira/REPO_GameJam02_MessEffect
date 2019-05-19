507aacd306887b6b5fbd71b8198d27495bbe699d
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-19 17:46:06 -0400

    Tested function to check disc/case match.

--- Assets/Scripts/GameDisplay.cs
+++ Assets/Scripts/GameDisplay.cs
@@ -6,9 +6,21 @@ public class GameDisplay : MonoBehaviour
 {
     public Game game;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
+    //}
+
+    /// <summary>
+    /// Check if the the disc inside this box belongs to it
+    /// </summary>
+    [ContextMenu("Check if match")]
+    public void CheckIfMatch() {
+        if (game.IsMatch()) {
+            Debug.Log("Great! This is the right disc!");
+        } else {
+            Debug.Log("This disc does not belong to this case"); 
+        }
     }
 }
