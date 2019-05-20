dcf181b8db2f81dafa3836c792fb69d2ea9c0e6a
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-20 12:37:41 -0400

    Started work on movement of the game case.

--- Assets/Scripts/GameDisplay.cs
+++ Assets/Scripts/GameDisplay.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GameDisplay : MonoBehaviour
 {
     public Game game;
+
+    public bool isDisplayed;
 
     //// Start is called before the first frame update
     //void Start()
@@ -23,4 +26,39 @@ public class GameDisplay : MonoBehaviour
             Debug.Log("This disc does not belong to this case"); 
         }
     }
+
+    // Testing the movement of the game case
+    [ContextMenu("Make game move")]
+    public void MakeGameMove() {
+        Sequence mySequence = DOTween.Sequence();
+
+        if (!isDisplayed) {
+            Vector3 target = transform.position + new Vector3(0, 0, -1.5f);
+            mySequence.Append(transform.DOMove(target, 2f));
+            mySequence.Append(transform.DOLocalRotate(new Vector3(0, 90f, 0), 2f));
+        } else {
+            Vector3 target = transform.position + new Vector3(0, 0, 1.5f);
+            mySequence.Append(transform.DOMove(target, 2f));
+            mySequence.Prepend(transform.DOLocalRotate(new Vector3(0, 0, 0), 2f));
+        }
+
+        
+        mySequence.Play();
+        isDisplayed = !isDisplayed;
+    }
+
+    //// Testing the movement of the game case
+    //[ContextMenu("Test Move")]
+    //public void TestMove()
+    //{
+    //    Tween myTween = transform.DOLocalRotate(new Vector3(0, 90f, 0), 2f).SetAutoKill(false);
+
+    //    if (isForward) {
+    //        myTween.Play();
+    //    } else {
+    //        myTween.Rewind();
+    //    }
+
+    //    isForward = !isForward;
+    //}
 }
