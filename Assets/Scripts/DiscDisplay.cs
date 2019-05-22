b6bff263d828644a5a6628f6bcc4af44fa7a4ee3
Vinicius Oliveira <vr.sena@gmail.com>
2019-05-22 12:45:13 -0400

    Disc canvas aligned with disc.

--- Assets/Scripts/DiscDisplay.cs
+++ Assets/Scripts/DiscDisplay.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DiscDisplay : MonoBehaviour
 {
     public Game gameDisc;
 
+    // UI Elements
+    public TextMeshProUGUI txtGameTitle;
+    public Image gameCover;
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-        
-    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the information from the game scriptable object and place it in the game object
+        txtGameTitle.text = gameDisc.gameName; //GameManager.GetInstance().gameDictionary[game.caseName];
+        gameCover.sprite = gameDisc.cover;
+    }
 
     //// Update is called once per frame
     //void Update()
     //{
-        
+
     //}
 }
